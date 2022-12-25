1aab265ca78173d417e7a359053273e8da101acc
Benjohn007 <johanben330@gmail.com>
2022-12-25 15:15:39 +0100

    new features

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -3,9 +3,6 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
-    const ClickHandler = () =>{
-        console.log('Clicked!!!');
-    };
     return(
         <Card className="expense-item">
            <ExpenseDate date={props.date}/>
@@ -13,7 +10,6 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-            <button onClick={ClickHandler}>Change Title</button>
         </Card>
     );
 }
